02a8a0df38abf6191c42e4f1c525f31177c9b025
hrisi14 <117001358+hrisi14@users.noreply.github.com>
2024-06-10 01:35:06 +0300

    Create .sh

---
+++ Shell tasks/2017-IN-02/.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+if [[ "$()" != "root" ]] ; then
+        echo "A non-root user can not do anything with this script!"
+        exit 0
+fi
+
+TMP_USERS=$(mktemp)
+
+while read current_user; do
+        user_dir="~${user}"   #Check whether correct!
+
+        if [[ ! -e "${user_dir}" ]] ; then
+                echo "${user}" >> "${TMP_USERS}"
+    fi
+
+        if [[ "$(stat -c '%U' "${user_dir}")" != "${user}" ]] ; then
+                echo "${user}" >> "${TMP_USERS}"
+        fi
+
+        if [[ ! "$(stat -c '%a' "${user_dir}")"&2 ]] ; then
+                echo "${user}" >> "${TMP_USERS}"
+        fi
+done < <(ps aux | grep -E -v "^root.*" | cut -d ' ' -f 1 | uniq)  #Do I need parenthesis here?
+
+
+root_total_rss="$(ps -u "root" -o rss= | awk '{sum += $1} END {print sum}')"
+uniq "${TMP_USERS}" "${TMP_USERS}"
+
+while read user_line ; do
+        total_rss="$(ps -u "${user_line}" -o rss= | awk '{sum += $1} END {print sum}')"
+        if [[ "${total_rss}" -gt "${root_total_rss}" ]] ; then
+                to_kill="$(ps -u "${user_line}" -o pid= --sort rss)"
+                kill "${to_kill}"
+                sleep 2
+                kill -KILL "${to_kill}"
+        fi
+done < "${TMP_USERS}"
+
+rm -f "${TMP_USERS}"
